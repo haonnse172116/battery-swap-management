6452491066c8b3fc6274b924fa07326c13d4d637
Hao Nguyen <haonnse172116@fpt.edu.vn>
2025-10-18 19:15:38 +0700

    feat: implement base api configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { apiSlice } from "../api/apiSlice";
 import authReducer from "./slices/authSlice";
 
-const persistConfig = {
-  key: "auth", 
+const authPersistConfig = {
+  key: "auth",
   storage,
 };
 
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // tắt warning về non-serializable
-    }),
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export const persistor = persistStore(store);
