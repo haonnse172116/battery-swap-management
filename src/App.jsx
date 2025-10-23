1756b924fad72910f1625faa7216c94873c6406e
LeBMinh <minhltbse173370@fpt.edu.vn>
2025-10-23 22:45:57 +0700

    feat: user list and demote/promote role

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routers";
 import GlobalErrorBoundary from "./components/ui/GlobalErrorBoundary";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <GlobalErrorBoundary>
       <RouterProvider router={router} />
+      <Toaster position="top-right" />
     </GlobalErrorBoundary>
   );
 }
